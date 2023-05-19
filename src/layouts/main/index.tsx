13f317f8beef05c74bd5359bd269ba6560dedd77
red-mat <jesus.ing.rodriguez@gmail.com>
2023-05-19 14:57:04 -0300

    init

---
+++ src/layouts/main/index.tsx
@@ -0,0 +1,13 @@
+export function Main() {
+  return (
+    <main className="flex flex-col items-center">
+      <h1 className="font-title text-">Main</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam saepe
+        recusandae laboriosam est eos hic dicta, sunt vel quasi dolorem quod
+        labore impedit neque. Adipisci reiciendis perferendis consequuntur.
+        Laboriosam, esse!
+      </p>
+    </main>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        title: ['Oswald', 'sans-serif']
+      },
+
+      colors: {
+        'primary-blue': 'rgb(var(--primary-blue))',
+        'secondary-blue': 'rgb(var(--secondary-blue))',
+      }
+    }
+  },
+  plugins: [],
+}
