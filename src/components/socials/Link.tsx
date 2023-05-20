cbcc5452bfcae19f6373725cf40b34d0a438b51e
red-mat <jesus.ing.rodriguez@gmail.com>
2023-05-19 21:22:00 -0300

    feat(components): socials
    Socials Link

---
+++ src/components/socials/Link.tsx
@@ -0,0 +1,22 @@
+import { FaFacebook, FaWhatsapp, FaGithub } from 'react-icons/fa'
+
+const SOCIAL_ICONS = {
+  FACEBOOK: FaFacebook,
+  WHATSAPP: FaWhatsapp,
+  GITHUB: FaGithub,
+}
+
+interface Props {
+  social: keyof typeof SOCIAL_ICONS
+  href: string
+}
+
+export function Link({ social, href }: Props) {
+  const Social = SOCIAL_ICONS[social]
+
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <Social className="text-secondary-blue hover:text-primary-blue max-h-5" />
+    </a>
+  )
+}
---
+++ src/components/socials/index.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react'
+import { Link } from './Link'
+
+interface Props {
+  children: ReactElement<typeof Link> | ReactElement<typeof Link>[]
+}
+
+function Socials({ children }: Props) {
+  if (!Array.isArray(children))
+    return (
+      <ul>
+        <li>{children}</li>
+      </ul>
+    )
+  return (
+    <ul className="flex px-1 h-fit py-0.5 bg-white border-2 border-primary-blue rounded-full justify-center items-center">
+      {children.map(function (Link, i) {
+        return (
+          <li key={i} className="mx-0.5 sm:mx-1">
+            {Link}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+Socials.Link = Link
+
+export { Socials }
