ca465942b766ad0ae965d531168ffee6745250e8
red-mat <jesus.ing.rodriguez@gmail.com>
2023-05-21 21:42:11 -0300

    feat(layouts): header
    Remove Navbar

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,3 +1,10 @@
+import Navbar from './Navbar'
+import Notify from './Notify'
+
 export function Header() {
-  return <header>Header</header>
+  return (
+    <header className="flex flex-col min-w-full h-[100vh] bg-banner bg-center bg-cover">
+      <Notify />
+    </header>
+  )
 }
