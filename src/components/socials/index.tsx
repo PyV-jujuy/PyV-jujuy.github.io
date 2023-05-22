68997d2a496c0955ae8bb2c0d846203a269de95d
red-mat <jesus.ing.rodriguez@gmail.com>
2023-05-21 21:28:03 -0300

    fix(components): socials
    add className social not array children case

--- src/components/socials/index.tsx
+++ src/components/socials/index.tsx
@@ -8,8 +8,8 @@ interface Props {
 function Socials({ children }: Props) {
   if (!Array.isArray(children))
     return (
-      <ul>
-        <li>{children}</li>
+      <ul className="flex px-1 h-fit py-0.5 bg-white border-2 border-primary-blue rounded-full justify-center items-center">
+        <li className="mx-0.5 sm:mx-1">{children}</li>
       </ul>
     )
   return (
