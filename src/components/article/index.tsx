371ed72282e7428f2d288d56bdeb88a0717cfea6
red-mat <jesus.ing.rodriguez@gmail.com>
2023-05-21 23:40:28 -0300

    feat(components): add Article component

---
+++ src/components/article/index.tsx
@@ -0,0 +1,22 @@
+import { Fragment, ReactElement } from 'react'
+import Section from './section'
+
+interface Props {
+  reverse?: boolean
+  children: [ReactElement<typeof Section>, ReactElement<typeof Section>]
+}
+function Article({ reverse = false, children }: Props) {
+  const flexReverse = reverse ? 'flex-row-reverse' : 'flex-row'
+
+  return (
+    <article className={`flex flex-col flex-auto md:w-3/4 md:${flexReverse}`}>
+      {children.map(function (a, i) {
+        return <Fragment key={i}> {a} </Fragment>
+      })}
+    </article>
+  )
+}
+
+Article.Section = Section
+
+export default Article
