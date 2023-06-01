6f1524a182de2eaa38a69499318fda5d40ae1026
red-mat <jesus.ing.rodriguez@gmail.com>
2023-06-01 18:02:18 -0300

    clean: project

--- src/components/article/section.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {
-  children: React.ReactNode
-}
-
-export default function Section({ children }: Props) {
-  return <section className="md:max-w-[50%] p-5">{children}</section>
-}
--- src/utils/const.tsx
+++
@@ -1,6 +0,0 @@
-export const ECONOMIC_PLAN =
-  'https://drive.google.com/file/d/14dsxnYKZ9EYarq1fYWeJYnwGBwwUKH2X/view'
-
-export const WHATSAPP_LINK = 'https://chat.whatsapp.com/HEGzXevVNdJ8Riq2IdOzBU'
-
-export const FACEBOOK_LINK = 'https://www.facebook.com/PartidoPyVJujuy/'
